565f28556d68ff4d8cf506c285f8a0616c8ee589
Stolarskis <stolarskis@g.cofc.edu>
2017-11-29 03:07:09 -0500

    Updated Parser, null terminator still a pain in the ass. Ill ask Munsell tomorrow.

--- ParseTest/parser.c
+++ ParseTest/parser.c
@@ -17,14 +17,14 @@
 
 
 main( int argc, char *argv[] ) { 
-
+	    //Reading the file and setting it equal to entity_body
             char fileNames[2000];
             char contents[4096];
-             char entity_body[4096];
-                    //This code is from the class c example 1
+             char entity_body[200000];
+        
             FILE* fhnd;
 
-            fhnd = fopen("text.txt", "r");
+            fhnd = fopen("post.txt", "r");
 
             char line[50];
 
@@ -45,43 +45,58 @@ main( int argc, char *argv[] ) {
 	
 	//=============================================================================
 	//Parsing the boundary
-
+	//printf("\nHere is the HTTP Message \n%s\n",entity_body);
 	
- 	char* finder  = strstr(entity_body,"boundary");
-	char* p1 = finder;
-	char* p2 = finder+9;              
+ 	char* finder  = strstr(entity_body,"boundary=");
+	char* findEnd = finder;
 	//printf("\n %s \n",finder);
 
 	for(;;){
-	if(*p1 == '\n'){
-	printf("\nFound the end of the line\n");
-	*p1 = '\0';
+	if(*findEnd == '\n'){
+	//printf("\nFound the end of the line\n");
+	*findEnd = '\0';
 	break;
 	}
-	p1++;
+	findEnd++;
 	}
-	char bound[100];
+	finder = finder+9;
 
-	strcpy(bound,p2); 
-	//printf("\n%s\n",bound);
-	*p1 = '-';
+	/**
+	size_t delimLen = strlen(finder);
+	char * bound = malloc(delimLen *4);
+	*/
+	char bound[200];
+	strcpy(bound,finder);
+	printf("\n Bound= \n%s\n",bound);
+	
+	//Getting the null Terminator
+	char nullBound[30000]; 
+	strcpy(nullBound,finder);
+	//strcpy(nullBound,bound);
+	char dashes[10];
+	strcpy(dashes,"-------");
+	strcat(nullBound,dashes);
+	printf("\nnullBound =\n%s\n",nullBound);
 
-
+	//Fuck it, I think basic strcat test
+	
+	char test1[100];
+	char test2[100];
+	strcpy(test1,"the dog walked");
+	strcpy(test2,"across the lake somehow");
+	strcat(test1,test2); 
+	printf("\n test1 = %s \n",test1);
+	*findEnd = '\n';
+	//Now here goes nothing in getting the nullTerminator
 
 	//============================================================================
-	//bound = seperator line
-	/**
-	char* token = strtok(entity_body,bound);
+	//Beginning of main parser, It uses two pointers (pp1, and pp2) to print and display
+	//what we want to see. It uses strstr to find the file, sets the end of the link equal 
+	//a null terminator and then moves the first pointer down a little to get rid of things
+	//like "filename=, and stuff like that". Its not very complex, but it also requires a lot
+	//of pen and paper to visualize.
 
-	while(token!= NULL){
-	printf("%s\n",token);
-
-	token = strtok(NULL,bound);
-	}	
-	*/
-
-
-	printf("\n----------------------------------------------------------------------\n");
+	//printf("\n----------------------------------------------------------------------\n");
 
   	char *delim = bound;
 	char* pp1 = entity_body;
@@ -94,16 +109,20 @@ main( int argc, char *argv[] ) {
 		pp2 = pp1;
 		for(;;){
 			if(*pp2 == '\n'){
+			pp2 = pp2 -2;
 			*pp2 = '\0';
 			break;
 			}
 		pp2++;
 		}
+		//Get rid of filename= (9 characters)
+		pp1 = pp1 + 10;
+	
 		//Print the line
-		printf("\n ====================================================== \n%s",pp1);
+		printf("\n ====================================================== \n%s\n",pp1);
 		//it Works, now to move the pp1 past content Type
-		pp1 = pp2+1;
-		//same thing as before but with pp1
+		pp1 = pp2+4;
+		//Basically this is a glorified, skip a line procedure 
 		for(;;){
 			if(*pp1 == '\n'){
 			pp1++;
@@ -111,16 +130,21 @@ main( int argc, char *argv[] ) {
 			}
 			pp1++;
 		}
-
+		//This is where we get the contents of the file
+		//We set pp2 = to the next boundary, and set it as a null terminator
     		 pp2 = strstr(pp1,delim);
     		 if (pp2){ 
+		pp2 = pp2 -2;
       		*pp2 = '\0';}
+		else{
+		break;
+		}
+		//Then its just a simple matter of printing pp1
 
-    		printf("\n %s\n",pp1);
-
+    		printf("%s\n",pp1);
+		//This was the tricky part,you set pp1 after the '\0' by making it equals to pp1
+		// + the length of the boundary
     		pp1 = pp2+strlen(delim);
 
   	}while(pp2!=NULL);
-
-
 }
